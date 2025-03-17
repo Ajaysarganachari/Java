c353ae2c303eaa536a3ff221c3039b2d86dfad75
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2025-03-17 11:44:52 +0530

    Create Sahil.java

---
+++ Sahil.java
@@ -0,0 +1,7 @@
+public class Sahil {
+    public static void main(String[] args) {
+        System.out.println("SARKAR GROUP");
+        System.out.println("😈😈Don Sahil Dada😈😈");
+        System.out.println("\"Maitri ha ek sundar bandh ahe jo lokanna asha margani jodto je samjavun sangne kathin aste. Sarkar Gatat, amhi vishwas, ekta aani adar yanna tikau natesambandhancha paya manto. Amcha gat maitri, sahakarya aani paraspar samarthanachya mulyanvar aadhaarit ahe, jithe pratyek sadasya aaplya adwitiya shaktiansah gatat yogdan deto. Ekatr, amhi saman uddishtankade kaam karto, yash saajare karto aani avhananmadhe shikto. Hech maitri che atma ahe jyamule amchya pragatila chalana milte aani gatamadhye ekmekanshi bandh majbut hotat.");
+    }
+}
