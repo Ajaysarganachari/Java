1c121ef41a1599708c6d2a8fd3820db082614ac4
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2025-03-17 11:11:29 +0530

    Create substraction.java

---
+++ substraction.java
@@ -0,0 +1,12 @@
+public class Num1 {
+    public static void main(String[] args) {
+        String num1=args[0];
+        String num2=args[1];
+        
+        int a=Integer.parseInt(num1);
+        int b=Integer.parseInt(num2);
+        
+        System.out.println("The addition="+(a-b));
+          System.out.println(a + " - " + b + " = " + (a - b)); 
+    }
+ }
