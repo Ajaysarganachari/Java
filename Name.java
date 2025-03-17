89a524622952b02059f834b0f76c7222cde6f04a
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2025-03-17 11:01:57 +0530

    Create Name.java

---
+++ Name.java
@@ -0,0 +1,11 @@
+public class New {
+    public static void main(String[] args) {
+        String my_name=args[0];
+        String my_age=args[1];
+        String my_clg=args[2];
+
+        System.out.println("my name="+my_name);
+        System.out.println("my age="+my_age);
+        System.out.println("my clg="+my_clg);
+    }
+}
