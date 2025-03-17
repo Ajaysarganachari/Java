1b762a4f4ef7dc775abd204de4bd78d216c8ddcc
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2025-03-17 11:02:49 +0530

    Create Java.java

---
+++ Java.java
@@ -0,0 +1,6 @@
+public class Java {
+   public static void main(String[] args) {
+    
+    System.out.println("Java is a high-level, object-oriented programming language developed by Sun Microsystems (now owned by Oracle Corporation). It is designed to be platform-independent and allows developers to write software that can run on any platform that has a Java Virtual Machine (JVM) installed. The defining features of Java include:");
+   }    
+}
