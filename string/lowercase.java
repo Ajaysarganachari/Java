6aa60fb0638d45dd659696b8e455b30134b73863
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2025-03-21 09:39:41 +0530

    Create lowercase.java

---
+++ string/lowercase.java
@@ -0,0 +1,9 @@
+package Day_04;
+
+public class Low {
+    public static void main(String[] args) {
+        String str="HELLO WORLD";
+        System.out.println(str.toLowerCase());
+    }
+    
+}
