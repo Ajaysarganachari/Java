453c745276f9a2a2657a31c0b5234f11e5a28288
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2025-03-21 09:29:58 +0530

    Create locate.java

---
+++ string/locate.java
@@ -0,0 +1,6 @@
+public class Locate {
+    public static void main(String[] args) {
+        String str = "Please locate where 'locat' occurs";
+        System.out.println(str.indexOf("locate"));
+    }
+}
