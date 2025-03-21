d97132ec39f3840b4323d09ba68192f788243936
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2025-03-21 10:09:48 +0530

    Create Wineding.java

---
+++ Casting/Wineding.java
@@ -0,0 +1,9 @@
+public class Wineding{
+    public static void main(String[] args){
+        int a=9;
+        double b=a;
+        System.out.println(a);
+        System.out.println(b);
+        
+    }
+}
