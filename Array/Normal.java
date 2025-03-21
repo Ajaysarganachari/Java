c1632a477ca04fb98b90ef4c2d2e240dda2ab649
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2025-03-21 10:19:32 +0530

    Create Normal.java

---
+++ Array/Normal.java
@@ -0,0 +1,15 @@
+public class Normal {
+    public static void main(String[] args) {
+        int i;
+        int arr[]=new int [5];
+        arr[0]=10;
+        arr[1]=20;
+        arr[2]=30;
+        arr[3]=40;
+        arr[4]=50;
+        System.out.println("normal for loop");
+        for(i=0;i<arr.length;i++)
+        System.out.println(arr[i]);
+    }
+    
+}
