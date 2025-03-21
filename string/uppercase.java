b9c47cd74f884bb2d211e767fa51e14d79efaca9
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2025-03-21 09:37:23 +0530

    Create uppercase.java

---
+++ string/uppercase.java
@@ -0,0 +1,9 @@
+//ouput uppercase made print hoil
+
+public class More {
+    public static void main(String[] args) {
+        String str="hello world";
+        System.out.println(str.toUpperCase());
+    }
+    
+}
