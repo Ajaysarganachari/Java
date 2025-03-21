e7a8baf6f98ca62370e9e8c5484f14b6ccaf9d86
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2025-03-21 10:25:33 +0530

    Create For each loop.java

---
+++ Array/For each loop.java
@@ -0,0 +1,15 @@
+public class Enhance {
+    public static void main(String[] args) {
+        int i;
+        int arr[]=new int [5];
+        arr[0]=10;
+        arr[1]=11;
+        arr[2]=12;
+        arr[3]=13;
+        arr[4]=14;
+        System.out.println("For each loop");
+        for(int a:arr)
+        System.out.println(a);
+    }
+    
+}
