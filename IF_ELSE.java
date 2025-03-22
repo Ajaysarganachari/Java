144913d3957f4b8f230a7075737592c8b47f3efb
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2025-03-22 11:28:31 +0530

    Create IF_ELSE.java

---
+++ IF_ELSE.java
@@ -0,0 +1,10 @@
+public class S3{
+    public static void main(String[] args){
+        int a=18;
+        int b=24;
+        if(a>30){
+            System.out.println("The number is greater than b");
+        }else
+        System.out.println("The number is greater than a");
+    }
+}
